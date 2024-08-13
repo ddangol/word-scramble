63ea5ac52ddb7fee65cebc72a40e78893cebfce5
ddangol <dangol.dishna@gmail.com>
2024-08-13 11:18:02 +0545

    up to timer

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
+inputField = document.querySelector("input"),
+refereshBtn =  document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer); 
+    timer = setInterval(() => {
+        if(maxTime > 0){
+            maxTime--;
+            return timeText.innerText = maxTime;
+
+        }
+
+        clearInterval(timer);
+        alert(`time's upppppppp!!! ${correctWord.toUpperCase()} was the correctWord}`)
+        initGame();
+    
+    },1000);
+
+}
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length )];
+    let wordArray = randomObj.word.split("");
+    // console.log(randomObj);
+    // console.log(wordArray);
+
+    for(let i = wordArray.length-1;i>0;i--){
+        let j = Math.floor(Math.random() * (i+1));//randomly getting the index
+    
+
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]; //swapping the letters
+    }
+
+    wordText.innerText = wordArray.join(""); //passes shuffled word as word text
+    hintText.innerText = randomObj.hint; //updates hint 
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);//clearing for the next input
+    console.log( randomObj);
+    
+}
+
+initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLocaleLowerCase();
+    if(!userWord) return alert("pplease enter a word");
+    if (userWord != correctWord) return alert (`oops! ${userWord} is incorrect.`);
+    alert(`hoooooooooohooh. ${userWord.toUpperCase()} is absolutely correct!!!`);//else is not needed due to the return statement exits the function
+    initGame();
+}
+
+refereshBtn.addEventListener("click", initGame);
+
+checkBtn.addEventListener("click", checkWord);
---
+++ js/words.js
@@ -0,0 +1,42 @@
+ const words = [
+    {
+      word: "addition",
+      hint: "The process of adding numbers"
+    },
+    {
+      word: "meeting",
+      hint: "Event in which people come together"
+    },
+    {
+      word: "number",
+      hint: "Math symbol used for counting"
+    },
+    {
+      word: "exchange",
+      hint: "The act of trading"
+    },
+    {
+      word: "journey",
+      hint: "Traveling from one place to another"
+    },
+    {
+      word: "recipe",
+      hint: "Instructions for cooking a dish"
+    },
+    {
+      word: "computer",
+      hint: "Electronic device for processing data"
+    },
+    {
+      word: "library",
+      hint: "Place where books are stored"
+    },
+    {
+      word: "celebrate",
+      hint: "To honor a special occasion"
+    },
+    {
+      word: "adventure",
+      hint: "Exciting or unusual experience"
+    }
+  ];
